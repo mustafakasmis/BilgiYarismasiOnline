c6020e33a9803724ff62260a8a0ee84b310ecfae
mustafakasmis <mustafa_kasmis38@hotmail.com>
2017-11-09 23:07:01 +0300

    Add files via upload

---
+++ Sinavlar/SporSinavSayfasi.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace BilgiYarismasiOnline.Sınavlar
+{
+    public partial class SporSinavSayfasi : System.Web.UI.Page
+    {
+
+        private KullaniciIslemleri k = new KullaniciIslemleri();
+
+        private string kullanici;
+
+        private int id;
+        private static int sayac=21;
+        private string cevap;
+        private string kullanicicevap;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            kullanici = Session["kadi"].ToString();
+
+            id = Convert.ToInt32(Session["sprid"].ToString());
+
+            k.sorularıGetir(id,sayac);
+
+            cevap = k.cevabiGetir(id, sayac);
+
+            soru.Text = k.soru;
+
+            btnSecenekA.Text = k.secenekA;
+            btnSecenekB.Text = k.secenekB;
+            btnSecenekC.Text = k.secenekC;
+            btnSecenekD.Text = k.secenekD;
+        }
+
+        protected void btnSecenekOncSoru_Click(object sender, EventArgs e)
+        {
+
+            btnSecenekA.BackColor = System.Drawing.Color.White;
+            btnSecenekB.BackColor = System.Drawing.Color.White;
+            btnSecenekC.BackColor = System.Drawing.Color.White;
+            btnSecenekD.BackColor = System.Drawing.Color.White;
+
+            sayac--;
+
+            if (sayac < 21)
+                Response.Write("<script>alert('ÖNCEKİ SORU BULUNAMADI');</script>");
+
+            id = Convert.ToInt32(Session["sprid"].ToString());
+            k.sorularıGetir(id, sayac);
+
+            soru.Text = k.soru;
+
+            btnSecenekA.Text = k.secenekA;
+            btnSecenekB.Text = k.secenekB;
+            btnSecenekC.Text = k.secenekC;
+            btnSecenekD.Text = k.secenekD;
+        }
+
+        protected void btnSecenekSnrSoru_Click(object sender, EventArgs e)
+        {
+
+            btnSecenekA.BackColor = System.Drawing.Color.White;
+            btnSecenekB.BackColor = System.Drawing.Color.White;
+            btnSecenekC.BackColor = System.Drawing.Color.White;
+            btnSecenekD.BackColor = System.Drawing.Color.White;
+
+            sayac++;
+
+            if (sayac > 22)
+                Response.Write("<script>alert('SONRAKİ SORU BULUNAMADI');</script>");
+
+            id = Convert.ToInt32(Session["sprid"].ToString());
+            k.sorularıGetir(id, sayac);
+
+            soru.Text = k.soru;
+
+            btnSecenekA.Text = k.secenekA;
+            btnSecenekB.Text = k.secenekB;
+            btnSecenekC.Text = k.secenekC;
+            btnSecenekD.Text = k.secenekD;
+        }
+
+        protected void btnSinaviBitir_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/KullaniciAnasayfa.aspx");
+        }
+
+        protected void btnSinavdanCik_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/KullaniciAnasayfa.aspx");
+        }
+
+
+        protected void btnSecenekA_Click(object sender, EventArgs e)
+        {
+            if (btnSecenekA.Text == cevap)
+            {
+                btnSecenekA.BackColor = System.Drawing.Color.Green;
+                kullanicicevap = btnSecenekA.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+
+            else
+            {
+                btnSecenekA.BackColor = System.Drawing.Color.Red;
+                kullanicicevap = btnSecenekA.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+        }
+
+        protected void btnSecenekB_Click(object sender, EventArgs e)
+        {
+            if (btnSecenekB.Text == cevap)
+            {
+                btnSecenekB.BackColor = System.Drawing.Color.Green;
+                kullanicicevap = btnSecenekB.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+
+            else
+            {
+                btnSecenekB.BackColor = System.Drawing.Color.Red;
+                kullanicicevap = btnSecenekB.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+
+        }
+
+        protected void btnSecenekC_Click(object sender, EventArgs e)
+        {
+            if (btnSecenekC.Text == cevap)
+            {
+                btnSecenekC.BackColor = System.Drawing.Color.Green;
+                kullanicicevap = btnSecenekC.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+
+            else
+            {
+                btnSecenekC.BackColor = System.Drawing.Color.Red;
+                kullanicicevap = btnSecenekC.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+
+        }
+
+        protected void btnSecenekD_Click(object sender, EventArgs e)
+        {
+            if (btnSecenekD.Text == cevap)
+            {
+                btnSecenekD.BackColor = System.Drawing.Color.Green;
+                kullanicicevap = btnSecenekD.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+
+            else
+            {
+                btnSecenekD.BackColor = System.Drawing.Color.Red;
+                kullanicicevap = btnSecenekD.Text;
+
+                k.kullaniciCevapEkle(kullanici, id, sayac, kullanicicevap);
+            }
+
+        }
+    }
+}
---
+++ Sinavlar/TarihSinavSayfasi.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TarihSinavSayfasi.aspx.cs" Inherits="BilgiYarismasiOnline.Sınavlar.TarihSinavSayfasi" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <title>TARİH SINAV SAYFASI</title>
+
+    <style type="text/css">
+
+        #lblSure{
+            color:#006600;
+            position:relative;
+            left:1000px;
+        }
+
+        #lblCevapKontrol{
+            color:#006600;
+            position:relative;
+            top:25px;
+            left:880px;
+        }
+
+    </style>
+
+</head>
+
+<body style="background-image:url('/images/0.jpg');">
+
+    <h2 style="color:rgb(53, 255, 0); text-align:center;">TARİH SINAV SAYFASI</h2>
+
+   
+    <form id="form1" runat="server">
+
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+
+        <div style="margin-top: 7px">
+
+            <asp:Panel ID="pnlsinav" runat="server" Height="423px" style="margin-top: 0px">
+
+                <<asp:TextBox ID="soru" runat="server" Height="167px" style="margin-left: 499px; margin-top: 82px;" TextMode="MultiLine" MaxLength="200" Width="360px"></asp:TextBox>
+                   
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <asp:Button ID="btnSecenekA" runat="server" Height="29px" style="margin-left: 276px" Text="" Width="152px" OnClick="btnSecenekA_Click" />
+                        <asp:Button ID="btnSecenekB" runat="server" Height="29px" style="margin-left: 70px" Text="" Width="152px" OnClick="btnSecenekB_Click"  />
+                        <asp:Button ID="btnSecenekC" runat="server" Height="29px" style="margin-left: 99px" Text="" Width="152px" OnClick="btnSecenekC_Click" />
+                        <asp:Button ID="btnSecenekD" runat="server" Height="29px" style="margin-left: 64px; margin-top: 0px;" Text="" Width="152px" OnClick="btnSecenekD_Click"  />
+                        
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+
+                <asp:Button ID="btnSecenekOncSoru" runat="server" Text="ÖNCEKİ SORU" style="margin-left: 448px" Font-Bold="True" Height="29px" Width="152px" Font-Italic="True" OnClick="btnSecenekOncSoru_Click" />
+                <asp:Button ID="btnSecenekSnrSoru" runat="server" Text="SONRAKİ SORU" style="margin-left: 147px" Font-Bold="True" Height="29px" Width="152px" Font-Italic="True" OnClick="btnSecenekSnrSoru_Click" />
+
+            </asp:Panel>
+
+
+           <asp:Panel ID="pnlSinavIslem" runat="server" style="margin-top: 32px" Height="90px">
+
+                <asp:Button ID="btnSinaviBitir" runat="server" Text="SINAVI BİTİR" Font-Bold="True" Font-Italic="False" style="margin-left: 450px; margin-top: 16px" Height="37px" Width="140px" OnClick="btnSinaviBitir_Click"  />
+                <asp:Button ID="btnSinavdanCik" runat="server" Text="SINAVDAN ÇIK" Font-Bold="True" Font-Italic="False" style="margin-left: 171px; margin-top: 63px" Height="37px" Width="140px" OnClick="btnSinavdanCik_Click" />
+               
+            </asp:Panel>
+
+        </div>
+
+    </form>
+
+</body>
+
+</html>
