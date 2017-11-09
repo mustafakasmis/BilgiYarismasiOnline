d71acaea1f9c799b0f27395f8b9a2c161f98b46d
mustafakasmis <mustafa_kasmis38@hotmail.com>
2017-11-09 23:09:56 +0300

    Add files via upload

---
+++ js/SiteAnasayfa.js
@@ -0,0 +1,19 @@
+﻿function trhKontrol() {
+
+    var gelen = document.getElementById("kontrol").innerHTML;
+
+    var tarih = gelen.substring(1, 5);
+
+    var donusumtarih = parseInt(tarih);
+
+    var gercektarih = new Date();
+
+    if (gercektarih.getFullYear() > donusumtarih) {
+
+        var aralik = gercektarih.getFullYear() - donusumtarih;
+        donusumtarih += aralik;
+        document.getElementById("kontrol").innerHTML = "&copy;" + donusumtarih + "-MUSTAFA KASMIŞ";
+
+    }
+
+}
